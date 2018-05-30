5cc030793e2de9daebc87324c88e5d18e39dfa2d
Rita Bennett-Chew and Will Swanson <will@>
2018-05-30 13:35:47 -0700

    complete dog and cat argument page, fixed js and css, confirmed working correctly

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $("ul#cat").prepend("<li>\"Meow\"</li>");
     $("ul#dog").prepend("<li>\"woof woof\"</li>");
   });
+  $("button#bark").click(function(){
+    $("ul#cat").prepend("<li>\"MROWWWW\"</li>");
+    $("ul#dog").prepend("<li>\"Bark\"</li>");
+  });
 });
