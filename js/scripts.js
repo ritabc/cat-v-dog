638e6f59918831dbcf971198c7b389409d2ad2a0
Rita Bennett-Chew and Will Swanson <will@>
2018-05-30 11:57:59 -0700

    Build cat page and add some dialogue between cat and dog

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,6 @@
 $(document).ready(function(){
-  $("button#hello").click(function(){
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>What's up, man?!</li>");
-
-    $("ul#user").children("li").first().click(function() {
-      $(this).remove();
-    });
-
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-    });
-  });
-  $("button#goodbye").click(function(){
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Later, dude!</li>");
-
-        $("ul#user").children("li").first().click(function() {
-          $(this).remove();
-        });
-
-        $("ul#webpage").children("li").first().click(function() {
-          $(this).remove();
-        });
-  });
-  $("button#stop").click(function(){
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>What do you mean?!</li>");
-
-        $("ul#user").children("li").first().click(function() {
-          $(this).remove();
-        });
-
-        $("ul#webpage").children("li").first().click(function() {
-          $(this).remove();
-        });
+  $("button#meow").click(function(){
+    $("ul#cat").prepend("<li>\"Meow\"</li>");
+    $("ul#dog").prepend("<li>\"woof woof\"</li>");
   });
 });
